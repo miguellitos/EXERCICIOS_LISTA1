0a79d5d99d25be970013b4342cb63509a115e387
miguellitos <128072135+miguellitos@users.noreply.github.com>
2023-09-18 16:49:00 +0000

    1 questão

--- .ccls-cache/@home@runner@ColorfulOrchidTerabyte/main.c
+++ .ccls-cache/@home@runner@ColorfulOrchidTerabyte/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  int ano;
-  return 0;
-}
+    int ano;
+
+    printf("Digite um ano (1800 <= ano <= 2022): ");
+    scanf("%d", &ano);
+
+    if (ano >= 1800 && ano <= 2022) {
+        if (ano >= 1896 && (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+            printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano);
+        } else if (ano >= 1930 && (ano % 4 == 0)) {
+            printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
+        } else {
+            printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
+        }
+    }
+
+    return 0;}
